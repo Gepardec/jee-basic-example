33da7c8c95556fa4e405d32935ea312eb4522965
Julian Gartner <julian.gartner@gmail.com>
2020-01-23 10:31:03 +0100

    setup empty project

--- src/main/java/com/gepardec/example/resource/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package com.gepardec.example.resource;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class ApplicationConfig extends Application {
-}
